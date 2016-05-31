a8b42e193ac61eb846c0217f6cf6636d5c43a3ce
Kévin Gossé <gossekevin1@gmail.com>
2016-05-31 16:38:41 +0200

    Add comments inside actions/todos.ts

--- src/actions/todos.ts
+++ src/actions/todos.ts
@@ -1,6 +1,16 @@
+/**
+ *  action types
+ */
+
+
 export const REMOVE_TODO = '@@todos/REMOVE_TODO';
 export const TOGGLE_ALL = '@@todos/TOGGLE_ALL';
 export const ADD_TODO = '@@todos/ADD_TODO';
 export const FILTER_REMAINING = '@@todos/FILTER_REMAINING';
 export const REMOVE_DONE_TASKS = '@@todos/REMOVE_DONE_TASKS';
 export const UPDATE_STORE = '@@todos/UPDATE_STORE';
+
+
+/**
+ *  action creators
+ */
