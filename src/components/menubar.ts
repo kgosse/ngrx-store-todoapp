4e29c9f1f7936a7be05a04e38b31d22f6d85573f
Kévin Gossé <gossekevin1@gmail.com>
2016-06-04 20:04:24 +0200

    Update menubar infos

--- src/components/menubar.ts
+++ src/components/menubar.ts
@@ -1,6 +1,8 @@
 import {Component} from "@angular/core";
 import {TodoService} from "../services/todo.service";
 import {StatusSelector} from "./status-selector";
+import {AppState} from "../interfaces/AppState";
+import {Store} from "@ngrx/store";
 
 @Component({
     selector: 'menubar',
@@ -18,21 +20,27 @@ import {StatusSelector} from "./status-selector";
       </style>
       <div id="menubar">
         <span id="todo-count">
-          <strong>{{getRemainingTasksLength()}}</strong> of <strong>{{getTodosLength()}} </strong>remaining
+          <strong>{{getRemainingTasksLength(todos | async)}}</strong> of <strong>{{getTodosLength(todos | async)}} </strong>remaining
         </span>
         <div>
           <input type="text" placeholder="Filter by text" #text (input)="textChange(text.value)"> &nbsp;&nbsp;
           <status-selector></status-selector>
         </div>
-        <a (click)="archive()" class="archive">Archive (<strong>{{getTodosLength() - getRemainingTasksLength()}}</strong>)</a>
+        <a (click)="archive()" class="archive">Archive (<strong>{{getTodosLength(todos | async) - getRemainingTasksLength(todos | async)}}</strong>)</a>
       </div>
     `
 })
 export class Menubar {
-    getTodosLength = () => this._todoService.getTodosLength();
-    getRemainingTasksLength = () => this._todoService.getRemainingTasksLength();
 
-    constructor(private _todoService: TodoService) {}
+    todos;
+    
+    getTodosLength = (v) => v ? v.length : 0;
+    getRemainingTasksLength = (v) => v ? v.filter(t => !t.done).length : 0;
+    
+
+    constructor(store: Store<AppState>, private _todoService: TodoService) {
+        this.todos = store.select('todos');
+    }
 
     archive() {
         this._todoService.archive();
